133dd3170a9a0854649bb766021f68cb9cd7d95a
Aryan Dinakaran <42404746+AryanDinakaran1@users.noreply.github.com>
2023-10-01 14:05:42 +0530

    Sign Up Built

--- Helpers.py
+++ Helpers.py
@@ -1,6 +1,53 @@
+import mysql.connector
+
+import Pages as pg
+
+# cur = con.connect_to_database()['cursor']
+
+try:
+    con = mysql.connector.connect(
+                host="localhost",  
+                user="root",       
+                password="password",
+                port=3306,         
+                database="mydb"
+            )
+    
+    cur = con.cursor()
+
+except:
+    print("ERROR CONNECTING TO DATABASE")
 
 def Encrypt():
     pass
 
 def Decrypt():
     pass
+
+def create_user(user_info):
+
+    cur.execute("SELECT * FROM User")
+    rows = cur.fetchall()
+
+    user_exists = False
+
+    for row in rows:
+        if user_info['email'] == row[3]:
+            user_exists = True
+
+    # CHECK IF USER ALREADY EXISTS
+    if user_exists:
+        print("USER ALREADY EXISTS\n")
+        pg.Authentication()
+
+    if rows == []:
+        set_id = 1
+
+    else:
+        set_id = rows[len(rows)-1][0] + 1
+
+    query = f"INSERT INTO User(id, name, password, email, phone_num, age, is_admin) VALUES ({set_id},'{user_info['name']}','{user_info['password']}','{user_info['email']}',{user_info['phone_num']},{user_info['age']},'{user_info['is_admin']}')"
+    cur.execute(query)
+    con.commit()
+
+    print("CREATED A NEW USER")
--- Pages.py
+++ Pages.py
@@ -1,15 +1,49 @@
-import Connection as con
 import Helpers as hlp
 
+
 def SignUp():
-    username = input("Enter Username: ")
+    name = input("Enter Name: ")
+    email = input("Enter Email: ")
+    phone_num = int(input("Enter Phone Number: "))
+    age = int(input("Enter Age: "))
     password = input("Enter Password: ")
 
-    query = ""
+    data = {
+        "name": name,
+        "password": password,
+        "email": email,
+        "phone_num": phone_num,
+        "age": age,
+        "is_admin": False
+    }
 
+    hlp.create_user(data)
+    Home()
+
+def SignIn():
+    pass
+
+def Authentication():
+
+    while True:
+        print("1] Sign Up")
+        print("2] Sign In")
+        print("3] EXIT")
+
+        opt = int(input("Choose Option Number: "))
+
+        if opt == 1:
+            SignUp()
+        elif opt == 2:
+            SignIn()
+        elif opt == 3:
+            break
+        else:
+            print("WRONG OPTION SELECTED\n")
+            Authentication()
 
 def Home():
-    pass
+    Authentication()
 
 if __name__ == '__main__':
     Home()
